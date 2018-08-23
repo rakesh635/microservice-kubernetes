aead4ff50745e6df4206fa5dcd810b952129c176
Thegaijin <ssemagandavictor@gmail.com>
2018-08-23 23:56:53 +0300

    this is a test

--- microservice-kubernetes-demo/kubernetes-deploy.sh
+++ microservice-kubernetes-demo/kubernetes-deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
-if [ -z "$DOCKER_ACCOUNT" ]; then
-    DOCKER_ACCOUNT=ewolff
-fi;
-kubectl run apache --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:latest --port=80
+
+kubectl run apache --image=thegaijin/demo-apache --port=80
 kubectl expose deployment/apache --type="LoadBalancer" --port 80
-kubectl run catalog --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:latest --port=8080
+kubectl run catalog --image=thegaijin/catalog --port=8080
 kubectl expose deployment/catalog --type="LoadBalancer" --port 8080
-kubectl run customer --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:latest --port=8080
+kubectl run customer --image=thegaijin/customer --port=8080
 kubectl expose deployment/customer --type="LoadBalancer" --port 8080
-kubectl run order --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-order:latest --port=8080
+kubectl run order --image=thegaijin/order --port=8080
 kubectl expose deployment/order --type="LoadBalancer" --port 8080
-kubectl run hystrix-dashboard --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-hystrix-dashboard:latest --port=8080
+kubectl run hystrix-dashboard --image=thegaijin/hystrix-dashboard --port=8080
 kubectl expose deployment/hystrix-dashboard --type="LoadBalancer" --port 8080
